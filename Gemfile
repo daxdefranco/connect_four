05883f6e0c53bea365883954e2a77d726ec5961e
daxdefranco <daxdefranco+git@gmail.com>
2013-11-15 15:08:16 +0100

    added simplecov gem

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'rake'
 gem 'minitest'
+gem 'simplecov', :require => false, :group => :test
--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -108,6 +108,7 @@ class ConnectFourTest < ConnectFourSpec
         # @game.get_drop_location(@new_array, input, @player).must ??? :put_player_token 
       end
 
+      #flag see note in game.rb:get_drop_location
       it "[get_drop_location] responds with :full if the column is full" do
         input = 4
         full = @new_array
@@ -115,77 +116,81 @@ class ConnectFourTest < ConnectFourSpec
         @game.get_drop_location(full, input, @player).must_equal :full
       end
       
-       # LEFT TO RIGHT WIN #
+    end
 
-        it "responds to 'left_right" do
-          @game.must_respond_to :left_right
-        end
+    describe "win conditions" do
+      
+      # LEFT TO RIGHT WIN #
 
-        it "[left_right] throws :winner with matching conditions" do
-          left_right_win = @new_array
-          1.upto(4) { |i| left_right_win[5][i] = 2 }
-          @game.left_right(left_right_win, 2).must_equal :winner
-        end
+       it "responds to 'left_right" do
+         @game.must_respond_to :left_right
+       end
 
-        it "[left_right] throws :nothing_yet without matching conditions" do
-          left_right_no_win = @new_array
-          1.upto(3) { |i| left_right_no_win[5][i] = 2 }
-          @game.left_right(left_right_no_win, 2).wont_equal :winner
-        end
+       it "[left_right] throws :winner with matching conditions" do
+         left_right_win = @new_array
+         1.upto(4) { |i| left_right_win[5][i] = 2 }
+         @game.left_right(left_right_win, 2).must_equal :winner
+       end
 
-        # DOWN TO UP WIN #
+       it "[left_right] throws :nothing_yet without matching conditions" do
+         left_right_no_win = @new_array
+         1.upto(3) { |i| left_right_no_win[5][i] = 2 }
+         @game.left_right(left_right_no_win, 2).wont_equal :winner
+       end
 
-        it "responds to 'down_up'" do
-          @game.must_respond_to :down_up
-        end
+       # DOWN TO UP WIN #
 
-        it '[down_up] throws :winner with matching conditions' do
-          down_up_win = @new_array
-          1.upto(4) { |i| down_up_win[i][3] = 2}
-          @game.down_up(down_up_win, 2).must_equal :winner
-        end
+       it "responds to 'down_up'" do
+         @game.must_respond_to :down_up
+       end
 
-        it "[down_up] throws :nothing_yet without matching conditions" do
-          down_up_no_win = @new_array
-          1.upto(3) { |i| down_up_no_win[i][3] = 2}
-          @game.down_up(down_up_no_win, 2).wont_equal :winner
-        end
+       it '[down_up] throws :winner with matching conditions' do
+         down_up_win = @new_array
+         1.upto(4) { |i| down_up_win[i][3] = 2}
+         @game.down_up(down_up_win, 2).must_equal :winner
+       end
 
-        # DIAGONAL RIGHT WIN #
+       it "[down_up] throws :nothing_yet without matching conditions" do
+         down_up_no_win = @new_array
+         1.upto(3) { |i| down_up_no_win[i][3] = 2}
+         @game.down_up(down_up_no_win, 2).wont_equal :winner
+       end
 
-        it "responds to diagonal_right" do
-          @game.must_respond_to :diagonal_right
-        end
+       # DIAGONAL RIGHT WIN #
 
-        it "[diagonal_right] throws :winner with matching conditions" do
-          diagonal_right_win = @new_array
-          0.upto(3) { |i| diagonal_right_win[6-i][1+i] = 2}
-          @game.diagonal_right(diagonal_right_win, 2).must_equal :winner
-        end
+       it "responds to diagonal_right" do
+         @game.must_respond_to :diagonal_right
+       end
 
-        it "[diagonal_right] throws :nothing_yet without matching conditions" do
-          diagonal_right_no_win = @new_array
-          0.upto(2) { |i| diagonal_right_no_win[6-i][1+i] = 2}
-          @game.diagonal_right(diagonal_right_no_win, 2).must_equal :nothing_yet
-        end
+       it "[diagonal_right] throws :winner with matching conditions" do
+         diagonal_right_win = @new_array
+         0.upto(3) { |i| diagonal_right_win[6-i][1+i] = 2}
+         @game.diagonal_right(diagonal_right_win, 2).must_equal :winner
+       end
 
-        #DIAGONAL LEFT WIN # 
+       it "[diagonal_right] throws :nothing_yet without matching conditions" do
+         diagonal_right_no_win = @new_array
+         0.upto(2) { |i| diagonal_right_no_win[6-i][1+i] = 2}
+         @game.diagonal_right(diagonal_right_no_win, 2).must_equal :nothing_yet
+       end
 
-        it "responds to diagonal_left" do
-          @game.must_respond_to :diagonal_left
-        end
+       #DIAGONAL LEFT WIN # 
 
-        it "[diagonal_left] throws :winner with matching conditions" do
-          diagonal_left_win = @new_array
-          0.upto(3) { |i| diagonal_left_win[6-i][7-i] = 2}
-          @game.diagonal_left(diagonal_left_win, 2).must_equal :winner
-        end
+       it "responds to diagonal_left" do
+         @game.must_respond_to :diagonal_left
+       end
 
-        it "[diagonal_left] throws :nothing_yet without matching conditions" do
-          diagonal_left_no_win = @new_array
-          0.upto(2) { |i| diagonal_left_no_win[6-i][7-i] = 2}
-          @game.diagonal_left(diagonal_left_no_win, 2).must_equal :nothing_yet
-        end
+       it "[diagonal_left] throws :winner with matching conditions" do
+         diagonal_left_win = @new_array
+         0.upto(3) { |i| diagonal_left_win[6-i][7-i] = 2}
+         @game.diagonal_left(diagonal_left_win, 2).must_equal :winner
+       end
+
+       it "[diagonal_left] throws :nothing_yet without matching conditions" do
+         diagonal_left_no_win = @new_array
+         0.upto(2) { |i| diagonal_left_no_win[6-i][7-i] = 2}
+         @game.diagonal_left(diagonal_left_no_win, 2).must_equal :nothing_yet
+       end
       
     end
 
@@ -196,12 +201,11 @@ class ConnectFourTest < ConnectFourSpec
     def setup
       #relax, nsa
       @c4 = ConnectFour
-      @game = Game.new
     end
 
-    # it "starts the game with an empty grid" do
-    #   proc {@c4.start }.must_output @game.start
-    # end
+    it "does something" do
+      
+    end
     
   end
 
